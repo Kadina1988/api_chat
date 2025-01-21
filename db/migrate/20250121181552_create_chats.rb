5b6bd2d22f61940105a531988cd6df73bb17e463
root <root@Cadi>
2025-01-21 22:14:23 +0300

    fix create_chats migration

--- db/migrate/20250121181552_create_chats.rb
+++ db/migrate/20250121181552_create_chats.rb
@@ -2,8 +2,8 @@ class CreateChats < ActiveRecord::Migration[8.0]
   def change
     create_table :chats do |t|
       t.timestamps
-      t.references :chats, :sender, null: false, foreign_key: { to_table: :users }
-      t.references :chats, :receiver, null: false, foreign_key: { to_table: :users }
+      t.references :sender, null: false, foreign_key: { to_table: :users }
+      t.references :receiver, null: false, foreign_key: { to_table: :users }
     end
   end
 end
